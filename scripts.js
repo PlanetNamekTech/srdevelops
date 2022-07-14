1cf9365c97729bc871644de8398afcbabbff58f1
PlanetNamekTech <sr23513@gmail.com>
2022-07-14 14:09:05 -0700

    added some more mobile responsiveness and navbar close on link click

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const navList = document.querySelectorAll('.nav__link');
+
 const selectElement = (s) => document.querySelector(s);
 //Open the menu navbar on click
 selectElement('.open').addEventListener('click', () => {
@@ -6,4 +8,8 @@ selectElement('.open').addEventListener('click', () => {
 //Close the menu on click
 selectElement('.close').addEventListener('click', () => {
   selectElement('.nav__list').classList.remove('active');
+})
+// Add close functionality when a link is clicked
+navList.forEach(link => {
+  link.addEventListener('click', () => selectElement('.nav__list').classList.remove('active'))
 })
